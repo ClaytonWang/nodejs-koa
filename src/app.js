b21529aad4289506f5b2ea785a4dbedfaebd9316
Clayton Wang <lovejunlic@qq.com>
2018-07-07 16:45:02 +0800

    mvc and rest

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const Koa = require('koa');
+
+const bodyParser = require('koa-bodyparser');
+
+const controller = require('./core/controller');
+
+const templating = require('./core/templating');
+
+const rest = require('./core/rest');
+
+const app = new Koa();
+
+// log request URL:
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    await next();
+});
+
+// static file support:
+let staticFiles = require('./core/static-files');
+app.use(staticFiles('/static/', __dirname + '/static'));
+
+// parse request body:
+app.use(bodyParser());
+
+// add nunjucks as view:
+app.use(templating('views', {
+    noCache: true,
+    watch: true
+}));
+
+// bind .rest() for ctx:
+app.use(rest.restify());
+
+// add controllers:
+app.use(controller());
+
+app.listen(3000);
+console.log('app started at port 3000...');
